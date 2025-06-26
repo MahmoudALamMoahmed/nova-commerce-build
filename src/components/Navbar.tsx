973f0a676a4a5ecd96acbce9d118096ed42495f0
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2025-06-26 03:39:19 +0300

    Edit User

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -103,10 +103,10 @@ const Navbar = ({ currentPath }: NavbarProps) => {
             
             {/* User Menu */}
             {user ? (
-              <DropdownMenu>
+              <DropdownMenu className="relative">
                 <DropdownMenuTrigger asChild>
                   
-                    <User className="h-6 w-6 p-2 text-gray-700 hover:text-brand-accent transition-colors relative" />
+                    <User className="h-6 w-6 p-2 text-gray-700 hover:text-brand-accent transition-colors" />
                   
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
