9b83d1d006386a9947adf2965986f7fe60949c29
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2025-06-26 05:53:29 +0300

    Edit Scroll

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Twitter, Mail, Phone, MapPin } from 'lucide-react'
 const Footer = () => {
   return (
     <footer className="bg-brand-DEFAULT text-white">
-      <div className="container mx-auto px-6 py-12">
+      <div className="container w-full px-6 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
           <div>
