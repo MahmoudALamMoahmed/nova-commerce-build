8672dc8ec92727ba5748f0329678536b66eb4fcc
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2025-08-12 04:03:19 +0300

    edit rtl admin

--- src/App.tsx
+++ src/App.tsx
@@ -44,25 +44,6 @@ const queryClient = new QueryClient({
 });
 
 function App() {
-  const { i18n } = useTranslation();
-
-  useEffect(() => {
-  const isAdminRoute = window.location.pathname.startsWith('/admin');
-  document.dir = isAdminRoute ? 'ltr' : (i18n.language === 'ar' ? 'rtl' : 'ltr');
-  document.documentElement.lang = i18n.language;
-}, [i18n.language]);
-
- useEffect(() => {
-  const handleLanguageChange = () => {
-    const isAdminRoute = window.location.pathname.startsWith('/admin');
-    document.dir = isAdminRoute ? 'ltr' : (i18n.language === 'ar' ? 'rtl' : 'ltr');
-    document.documentElement.lang = i18n.language;
-  };
-
-     i18n.on('languageChanged', handleLanguageChange);
-  return () => i18n.off('languageChanged', handleLanguageChange);
-}, [i18n]);
-
   return (
     <Router>
       <QueryClientProvider client={queryClient}>
